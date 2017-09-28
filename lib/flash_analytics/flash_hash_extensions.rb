b5098f54cb8d6622ad7b54074a4d15a63722de77
fylooi <fy.looi@gmail.com>
2017-09-28 14:32:06 +0800

    add non now version

--- lib/flash_analytics/flash_hash_extensions.rb
+++ lib/flash_analytics/flash_hash_extensions.rb
@@ -9,11 +9,18 @@ module FlashAnalytics
     end
 
     # stores the partial name and any local variables in the flash['flash_analytics'] hash
+    # will be displayed on the next request
     def add_analytics_partial partial, *locals_hash
+      self["flash_analytics"] ||= {}
+      self["flash_analytics"][partial] = locals_hash[0]
+    end
+    
+    # stores the partial name and any local variables in the flash['flash_analytics'] hash
+    # will be displayed on the current request
+    def add_analytics_partial_now partial, *locals_hash
       now["flash_analytics"] ||= {}
       now["flash_analytics"][partial] = locals_hash[0]
     end
-
     # exclude flash['analytics'] from flash.each display operations
     def each(&block)
       @flashes.reject {|k,v| k.to_s == "flash_analytics"}.each(&block)
